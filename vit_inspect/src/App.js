4d6439cd3b1e421fccd90ed0a3cb954afea1e202
Stefanos Stamatiadis <stamatiad.st@gmail.com>
2022-12-19 20:23:24 +0200

    Adds query pixel selector.

--- vit_inspect/src/App.js
+++ vit_inspect/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import $ from "jquery";
 // The import statement is for the webpack to know the dependence on
 // external js/css files and to add them to the compiled output.
 import '../static/js/bootstrap.min.js'
@@ -9,6 +10,7 @@ class TbImage extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            key: 0,
             layer: 1,
             head: 1,
             src: "individualImage?blob_key=WyIiLCJpbWFnZXMiLCIuIiwiVmlUMTYiLDAsNV0"
@@ -22,6 +24,161 @@ class TbImage extends React.Component{
         return (
             <div className="" id="weights-img">
                 <img className="img-thumbnail" src={this.state.src}/>
+            </div>
+        );
+    }
+}
+/*
+//className="pixelq-col"
+class Row extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("ROW CONSTRUCTOR");
+    }
+    render() {
+        console.log("ROW RENDER");
+        return (
+            <div className="row g-0">
+                That's a row, yo!
+                {this.props.children}
+            </div>
+        );
+    }
+};
+class Col extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("COL CONSTRUCTOR");
+    }
+    render() {
+        console.log("COL RENDER");
+        return (<div>I'm a column!</div>);
+    }
+};
+
+ */
+class GridCell extends React.Component {
+    // That's why:
+    //https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+    constructor(props) {
+        super(props);
+        this.state= {
+            l: props.layer,
+            h: props.head,
+        };
+    }
+
+    click = () => {
+        this.props.select(this.state.l, this.state.h);
+    }
+
+    render() {
+        return (
+            <div
+                onClick={this.click}
+                style={
+                {gridArea: `${this.state.l} ${this.state.h} ${this.state.l} ${this.state.h}`}
+            }>
+            </div>
+        );
+    }
+}
+
+class TbPixelQuery extends React.Component {
+    constructor(props) {
+        super(props);
+        // TODO: State needs to know what image/batch is this.
+        this.state = {
+            image_id: 0,
+            layers: props.layers,
+            heads: props.heads,
+            grid: [],
+            src: "individualImage?blob_key=WyIiLCIuIiwiVmlUMTYiLDAsNF0",
+        };
+        // Generate the styles to be used in grid:
+        this.GridStyle = {
+            display: "grid",
+            gridTemplateRows: "repeat("+this.state.layers+", 1fr)",
+            gridTemplateColumns: "repeat("+this.state.heads+", 1fr)"
+        }
+    }
+
+
+
+    componentDidMount() {
+        //this.createQueryGrid();
+    }
+
+    selectQueryPixel(layer, head) {
+        console.log(`Query pixel is ${layer} ${head}`);
+
+    }
+
+    Grid () {
+        var grid = [];
+
+        for (let l=0; l < this.state.layers; l++) {
+            for (let h=0; h < this.state.heads; h++) {
+                grid.push(
+                    <GridCell layer={l} head={h} select={this.selectQueryPixel}/>
+                );
+            }
+        }
+        return grid;
+    };
+
+
+    createQueryGrid() {
+        // Create the grid to enable pixel query visualization.
+        function Row(props) {
+            return (
+                <div className="row g-0">
+                    {props.children}
+                </div>
+            );
+        };
+        function Col() {
+            return (
+                <div className="pixelq-col">
+
+                </div>
+            );
+        };
+        var grid = [];
+        //var col = (<Col key={0}/>);
+
+        for (let l=0; l < this.state.layers; l++) {
+            var tmp = [];
+            for (let h=0; h < this.state.heads; h++) {
+                tmp.push(
+                    <Col key={h}/>
+                );
+            }
+            grid.push(
+                <Row key={l}>{tmp}</Row>
+            );
+        }
+
+        // Update the state with the created grid:
+        this.setState(
+            {
+                grid: grid
+            }
+        );
+    }
+
+    render(){
+        return (
+            <div>
+                <span className="fs-5">Pixel Query</span>
+                <div className="pixelq-img" id="weights-img">
+                    <img className="img-thumbnail" src={this.state.src}/>
+                    <div className="pixelq-grid" style={this.GridStyle}>
+                        {
+                            this.Grid()
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
@@ -42,7 +199,7 @@ function TbSidebar() {
     <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark"
          id={"sidebar"}>
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <span className="fs-4">Method</span>
+        <span className="fs-5">Visualization Method</span>
       </a>
       <hr/>
         <div>
@@ -70,6 +227,11 @@ function TbSidebar() {
                 </div>
             </div>
         </div>
+        <hr/>
+        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <span className="fs-5">Parameters</span>
+        </a>
+        <TbPixelQuery layers={7} heads={7}/>
       <ul className="nav nav-pills flex-column mb-auto">
         <li>
           <a href="#" className="nav-link text-white">
@@ -89,7 +251,7 @@ function TbVizualization() {
     const arr = [];
     for (let l=0; l < 4; l++) {
         for (let h=0; h < 4; h++) {
-            arr.push(<TbImage layer={l} head={h}/>);
+            arr.push(<TbImage key={l*12+h} layer={l} head={h}/>);
         }
     }
     return (
@@ -102,12 +264,41 @@ function TbVizualization() {
     );
 }
 
-function App() {
-  return (
-    <div className="App">
-          <TbDashboardLayout/>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        //this.addDebugger();
+    }
+
+    addDebugger() {
+        //This fails because I can not get pass the CSP...
+        var debuger_url = "http://localhost:8097";
+        $.ajax(debuger_url, {
+            method: "GET",
+            crossDomain: true,
+            contentType: "text/javascript",
+            dataType: "json",
+            xhrFields: {
+              withCredentials: true
+            }
+            //headers: {'nonce': "blahdeblahsadf"}, // This is the magic line
+        }).done(function (response) {
+            console.log("PARAMS FETCHED SUCCESSFULLY!")
+        }).fail(function (error) {
+            console.log('Something went WRONG in fetching the dxf PARAMS from the server.', error);
+            console.log(error);
+        });
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <TbDashboardLayout/>
+            </div>
+        );
+    }
 }
 
 export default App;
