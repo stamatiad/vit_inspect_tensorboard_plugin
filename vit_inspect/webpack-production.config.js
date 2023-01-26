a7c73af6076848b24250b5f9cfda8c5bf4461931
Stefanos Stamatiadis <stamatiad.st@gmail.com>
2023-01-26 13:00:39 +0200

    Adds production config and build.

---
+++ vit_inspect/webpack-production.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    // DXF editor only uses js on the front-end:
+    target: "web",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".js",".jsx",".ts", ".tsx"],
+        fallback: {
+            fs: false,
+            path: false,
+            crypto: false
+        }
+    },
+    watch: false,
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/static/js',
+        // Webpack exports the project as module. This is experimental:
+        // https://github.com/webpack/webpack/issues/2933#issuecomment-774253975
+        // Don't blame me if it breaks
+        //globalObject: 'this',
+        library: {
+            type: "module",
+            //export: "default"
+        },
+    },
+    experiments: {
+        outputModule: true
+    },
+    module: {
+       rules: [
+           {
+               test: /\.(js|ts)x?$/,
+               exclude: /node_modules/,
+               use: {
+                   loader: "babel-loader"
+               }
+           },
+           {
+               test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+           }
+       ]
+   }
+}
